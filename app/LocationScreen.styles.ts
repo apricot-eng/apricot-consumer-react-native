d3db601d542d647aa72721c52b6c56a114f097c1
Lucas Gramajo <lucasgramajo@gmail.com>
2026-01-19 18:04:06 -0500

    Using theme colours on all remaining styles.

--- app/LocationScreen.styles.ts
+++ app/LocationScreen.styles.ts
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native';
 import { FigmaColors } from '@/constants/theme';
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
--- styles/app/_layout.styles.ts
+++ styles/app/_layout.styles.ts
@@ -1,11 +1,13 @@
 import { StyleSheet } from 'react-native';
 
+import { FigmaColors } from '@/constants/theme';
+
 export const styles = StyleSheet.create({
   loadingContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: FigmaColors.white,
   },
 });
 
