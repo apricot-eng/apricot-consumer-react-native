6c8123ddbe9478e622fd2fa00a05148128177542
Lucas Gramajo <lucasgramajo@gmail.com>
2026-01-19 17:14:19 -0500

    Added tests

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'jest-expo',
+  watchman: false,
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.expo/**',
+    '!**/coverage/**',
+  ],
+};
